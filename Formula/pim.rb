afb9339630d81ac603bee3335bdbe7ce2904ed73
Ihor Vovk <adwolf15@gmail.com>
2025-11-13 09:14:51 +0100

    Change repository address

--- Formula/pim.rb
+++ Formula/pim.rb
@@ -1,7 +1,7 @@
 class Pim < Formula
   desc "CLI to manage AI instructions and prompts with ease"
-  homepage "https://github.com/hubblew/pim"
-  url "https://github.com/hubblew/pim/archive/refs/tags/v0.1.1.tar.gz"
+  homepage "https://github.com/hubblew/pim-cli"
+  url "https://github.com/hubblew/pim-cli/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "9fa407d3cc273d3947c65d58aa55d59f95e8373057830ca99e96521589066ccd"
   license "Apache-2.0"
 
